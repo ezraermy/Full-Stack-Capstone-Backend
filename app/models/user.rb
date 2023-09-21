a6724cd17ba5675e93da0ebe2d7174af9a1349e0
Pablo <ezekielgonzalezr@gmail.com>
2023-09-21 12:19:47 -0600

    Fix indentation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
- has_many :reservations
+  has_many :reservations
 
- validates :name, presence: true
- validates :user_name, presence: true
+  validates :name, presence: true
+  validates :user_name, presence: true
 end
