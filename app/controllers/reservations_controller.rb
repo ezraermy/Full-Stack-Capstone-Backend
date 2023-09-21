b25408694280e96f0c0a7fd126735a009585a13b
Ezra <ezraermy@gmail.com>
2023-09-21 19:53:49 +0300

    Create reservation controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,47 @@
 class ReservationsController < ApplicationController
+  before_action :set_reservation, only: %i[show edit update destroy]
+
+  def index
+    @reservations = Reservation.all
+  end
+
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    if @reservation.save
+      redirect_to @reservation, notice: 'Reservation was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @reservation.update(reservation_params)
+      redirect_to @reservation, notice: 'Reservation was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @reservation.destroy
+    redirect_to reservations_url, notice: 'Reservation was successfully deleted.'
+  end
+
+  private
+
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:user_id, :car_id, :reservation_date, :due_date, :service_fee)
+  end
 end
