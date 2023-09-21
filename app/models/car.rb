5420b2fa3089957fffe39065d713c0ae2f02cab3
yosaddis <email.to.yoseph@gmail.com>
2023-09-21 21:21:10 +0300

    Add Car Model Validations

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,4 +2,9 @@ class Car < ApplicationRecord
   has_many :reservations, dependent: :destroy
   has_many :users, through: :reservations
 
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :location, presence: true
+  validates :daily_rate, presence: true, numericality: { greater_than: 0 }
+  validates :car_type, presence: true
 end
