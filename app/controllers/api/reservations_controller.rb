32ef079fd016fa887a9c29271029e912794c7c47
Ezra <ezraermy@gmail.com>
2023-09-22 01:01:58 +0300

    Fix rubocop

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -1,55 +1,53 @@
 class Api::ReservationsController < ApplicationController
-      before_action :set_reservation, only: %i[show update destroy]
-      skip_before_action :verify_authenticity_token, only: %i[destroy create]
-  
-      # HTTP GET request to retrieve a list of reservations.
-      def index
-        @reservations = Reservation.includes(:car).all
-        render json: { reservations: @reservations }, status: :ok
-      end
+  before_action :set_reservation, only: %i[show update destroy]
+  skip_before_action :verify_authenticity_token, only: %i[destroy create]
 
-      # HTTP POST request to create a new reservation
-      def create
-        @user = User.find_by(username: params[:username])
-        @reservation = @user.reservations.build(reservation_params)
-        if @reservation.save
-          render json: @reservation, status: :created
-        else
-          render json: { errors: @reservation.errors.full_messages }, status: :unprocessable_entity
-        end
-      end
-  
-      # HTTP GET request to retrieve details of a specific reservation
-      def show
-        render json: Reservation.new(@reservation).as_json
-      end
-  
-      # HTTP PUT request to update an existing reservation
-      def update
-        if @reservation.update(reservation_params)
-          render json: Reservation.new(@reservation).as_json , status: :ok
-        else
-          render json: { errors: @reservation.errors.full_messages }, status: :unprocessable_entity
-        end
-      end
-  
-      # DELETE /api/reservations/:id
-      def destroy
-        @reservation.destroy
-        head :no_content
-      end
-  
-      private
-  
-      # Use callbacks to share common setup or constraints between actions.
-      def set_reservation
-        @reservation = Reservation.find(params[:id])
-      end
-  
-      # Only allow a list of specific parameters for create and update actions.
-      def reservation_params
-        params.require(:reservation).permit(:username, :reservation_date, :due_date, :service_fee, :car_id)
-      end
+  # HTTP GET request to retrieve a list of reservations.
+  def index
+    @reservations = Reservation.includes(:car).all
+    render json: { reservations: @reservations }, status: :ok
+  end
+
+  # HTTP POST request to create a new reservation
+  def create
+    @user = User.find_by(username: params[:username])
+    @reservation = @user.reservations.build(reservation_params)
+    if @reservation.save
+      render json: @reservation, status: :created
+    else
+      render json: { errors: @reservation.errors.full_messages }, status: :unprocessable_entity
     end
   end
-  
+
+  # HTTP GET request to retrieve details of a specific reservation
+  def show
+    render json: Reservation.new(@reservation).as_json
+  end
+
+  # HTTP PUT request to update an existing reservation
+  def update
+    if @reservation.update(reservation_params)
+      render json: Reservation.new(@reservation).as_json, status: :ok
+    else
+      render json: { errors: @reservation.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /api/reservations/:id
+  def destroy
+    @reservation.destroy
+    head :no_content
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+
+  # Only allow a list of specific parameters for create and update actions.
+  def reservation_params
+    params.require(:reservation).permit(:username, :reservation_date, :due_date, :service_fee, :car_id)
+  end
+end
