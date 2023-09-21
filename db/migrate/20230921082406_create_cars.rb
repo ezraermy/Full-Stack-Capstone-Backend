123273a054670a538128d4d60890876fb0b19487
Ezra <ezraermy@gmail.com>
2023-09-21 11:24:27 +0300

    Create a setup

---
+++ db/migrate/20230921082406_create_cars.rb
@@ -0,0 +1,13 @@
+class CreateCars < ActiveRecord::Migration[7.0]
+  def change
+    create_table :cars do |t|
+      t.string :name
+      t.text :description
+      t.string :location
+      t.decimal :daily_rate
+      t.string :car_type
+
+      t.timestamps
+    end
+  end
+end
