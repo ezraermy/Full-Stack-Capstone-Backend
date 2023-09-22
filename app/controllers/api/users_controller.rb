92235e4a75a4acc4524809650089592caf24abcc
Pablo <ezekielgonzalezr@gmail.com>
2023-09-21 20:58:24 -0600

    Fix linters

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,20 +1,22 @@
 class Api::UsersController < ApplicationController
   def index
     @user = User.find_by(user_name: params[:user_name])
-    if @user 
-        render json: @user
-    else 
-        render json:  { errors: "User doesn't exist" }
+    if @user
+      render json: @user
+    else
+      render json: { errors: "User doesn't exist" }
     end
   end
+
   def create
     new_user = User.new(user_params)
-    if new_user.save 
-        render json: new_user
-    else 
-        render json:  { errors: "Could not create user" }
+    if new_user.save
+      render json: new_user
+    else
+      render json: { errors: 'Could not create user' }
     end
   end
+
   def user_params
     params.require(:user).permit(:name, :user_name)
   end
