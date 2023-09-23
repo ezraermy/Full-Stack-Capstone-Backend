313efcd545e93fcebad4f2e28b195e2de43ccda5
Pablo <ezekielgonzalezr@gmail.com>
2023-09-23 17:08:52 -0600

    Fix rubocop offenses

--- spec/requests/api/cars_spec.rb
+++ spec/requests/api/cars_spec.rb
@@ -1,26 +1,24 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/cars', type: :request do
-
   path '/api/cars' do
-    
     get('list cars') do
       produces 'application/json'
       response(200, 'successful') do
         schema type: :array,
-        items: {
-          type: :object,
-          properties: {
-            id: { type: :integer },
-            name: { type: :string },
-            description: { type: :string },
-            image: { type: :string },
-            location: { type: :string },
-            daily_rate: { type: :number, format: :float },
-            car_type: { type: :string },
-          },
-          required: ['id', 'name', 'description', 'image', 'location', 'daily_rate', 'car_type']
-        }
+               items: {
+                 type: :object,
+                 properties: {
+                   id: { type: :integer },
+                   name: { type: :string },
+                   description: { type: :string },
+                   image: { type: :string },
+                   location: { type: :string },
+                   daily_rate: { type: :number, format: :float },
+                   car_type: { type: :string }
+                 },
+                 required: %w[id name description image location daily_rate car_type]
+               }
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
@@ -42,21 +40,21 @@ RSpec.describe 'api/cars', type: :request do
           car_type: { type: :string },
           image: { type: :string },
           location: { type: :string },
-          daily_rate: { type: :number, format: :float },
+          daily_rate: { type: :number, format: :float }
         },
-        required: ['name', 'car_type', 'image', 'location', 'daily_rate']
+        required: %w[name car_type image location daily_rate]
       }
       response(200, 'successful') do
         schema type: :object,
-          properties: {
-            id: { type: :integer },
-            name: { type: :string },
-            car_type: { type: :string },
-            image: { type: :string },
-            location: { type: :string },
-            daily_rate: { type: :number, format: :float },
-          },
-          required: ['name', 'car_type', 'image', 'location', 'daily_rate']
+               properties: {
+                 id: { type: :integer },
+                 name: { type: :string },
+                 car_type: { type: :string },
+                 image: { type: :string },
+                 location: { type: :string },
+                 daily_rate: { type: :number, format: :float }
+               },
+               required: %w[name car_type image location daily_rate]
 
         after do |example|
           example.metadata[:response][:content] = {
@@ -69,10 +67,10 @@ RSpec.describe 'api/cars', type: :request do
       end
       response(404, 'could not create car') do
         schema type: :object,
-        properties: {
-          errors: { type: :string }
-        },
-        required: ['errors']
+               properties: {
+                 errors: { type: :string }
+               },
+               required: ['errors']
         run_test! do |response|
           data = JSON.parse(response.body)
           expect(data).to be_an(Object)
@@ -90,15 +88,15 @@ RSpec.describe 'api/cars', type: :request do
       response(200, 'successful') do
         let(:id) { '123' }
         schema type: :object,
-          properties: {
-            id: { type: :integer },
-            name: { type: :string },
-            car_type: { type: :string },
-            image: { type: :string },
-            location: { type: :string },
-            daily_rate: { type: :number, format: :float },
-          },
-          required: ['name', 'car_type', 'image', 'location', 'daily_rate']
+               properties: {
+                 id: { type: :integer },
+                 name: { type: :string },
+                 car_type: { type: :string },
+                 image: { type: :string },
+                 location: { type: :string },
+                 daily_rate: { type: :number, format: :float }
+               },
+               required: %w[name car_type image location daily_rate]
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
@@ -110,16 +108,15 @@ RSpec.describe 'api/cars', type: :request do
       end
     end
 
-
     delete('delete car') do
       produces 'application/json'
       response(200, 'successful') do
         let(:id) { '123' }
         schema type: :object,
-        properties: {
-          message: { type: :string }
-        },
-        required: ['message']
+               properties: {
+                 message: { type: :string }
+               },
+               required: ['message']
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
@@ -131,10 +128,10 @@ RSpec.describe 'api/cars', type: :request do
       end
       response(404, 'Could not delete reservation') do
         schema type: :object,
-        properties: {
-          errors: { type: :string }
-        },
-        required: ['errors']
+               properties: {
+                 errors: { type: :string }
+               },
+               required: ['errors']
         run_test! do |response|
           data = JSON.parse(response.body)
           expect(data).to be_an(Object)
--- spec/requests/api/users_spec.rb
+++ spec/requests/api/users_spec.rb
@@ -1,9 +1,7 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/users', type: :request do
-
   path '/api/users' do
-
     get('user information') do
       consumes 'application/json'
       produces 'application/json'
@@ -12,18 +10,18 @@ RSpec.describe 'api/users', type: :request do
         properties: {
           user_name: { type: :string }
         },
-        required: ['name', 'user_name']
+        required: %w[name user_name]
       }
       response(200, 'successful') do
         schema type: :object,
-        properties: {
-          name: { type: :string },
-          user_name: { type: :string },
-          id: { type: :integer },
-          created_at: { type: :string, format: :'date-time' },
-          updated_at: { type: :string, format: :'date-time' }
-        },
-        required: ['name', 'user_name', 'id']
+               properties: {
+                 name: { type: :string },
+                 user_name: { type: :string },
+                 id: { type: :integer },
+                 created_at: { type: :string, format: :'date-time' },
+                 updated_at: { type: :string, format: :'date-time' }
+               },
+               required: %w[name user_name id]
         run_test! do |response|
           data = JSON.parse(response.body)
           expect(data).to be_an(Object)
@@ -32,10 +30,10 @@ RSpec.describe 'api/users', type: :request do
 
       response(404, 'user not found') do
         schema type: :object,
-        properties: {
-          errors: { type: :string }
-        },
-        required: ['errors']
+               properties: {
+                 errors: { type: :string }
+               },
+               required: ['errors']
         run_test! do |response|
           data = JSON.parse(response.body)
           expect(data).to be_an(Object)
@@ -52,31 +50,31 @@ RSpec.describe 'api/users', type: :request do
           name: { type: :string },
           user_name: { type: :string }
         },
-        required: ['name', 'user_name']
+        required: %w[name user_name]
       }
 
       response(200, 'successful') do
         schema type: :object,
-        properties: {
-          name: { type: :string },
-          user_name: { type: :string },
-          id: { type: :integer },
-          created_at: { type: :string, format: :'date-time' },
-          updated_at: { type: :string, format: :'date-time' }
-        },
-        required: ['name', 'user_name', 'id'] 
+               properties: {
+                 name: { type: :string },
+                 user_name: { type: :string },
+                 id: { type: :integer },
+                 created_at: { type: :string, format: :'date-time' },
+                 updated_at: { type: :string, format: :'date-time' }
+               },
+               required: %w[name user_name id]
         run_test! do |response|
-        data = JSON.parse(response.body)
-        expect(data).to be_an(Object)
+          data = JSON.parse(response.body)
+          expect(data).to be_an(Object)
         end
       end
 
       response(404, 'could not create user') do
         schema type: :object,
-        properties: {
-          errors: { type: :string }
-        },
-        required: ['errors']
+               properties: {
+                 errors: { type: :string }
+               },
+               required: ['errors']
         run_test! do |response|
           data = JSON.parse(response.body)
           expect(data).to be_an(Object)
