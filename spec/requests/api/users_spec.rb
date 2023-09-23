b3ea40ef69adb2599ef4cef5775e901747806905
Pablo <ezekielgonzalezr@gmail.com>
2023-09-23 15:23:44 -0600

    Improve users api documentation

--- spec/requests/api/users_spec.rb
+++ spec/requests/api/users_spec.rb
@@ -70,6 +70,18 @@ RSpec.describe 'api/users', type: :request do
         expect(data).to be_an(Object)
         end
       end
+
+      response(404, 'could not create user') do
+        schema type: :object,
+        properties: {
+          errors: { type: :string }
+        },
+        required: ['errors']
+        run_test! do |response|
+          data = JSON.parse(response.body)
+          expect(data).to be_an(Object)
+        end
+      end
     end
   end
 end
